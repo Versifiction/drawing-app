5a4915ff3c990ee62e217dd9e28c07c1e5580c56
Versifiction <marc_charpentier@hotmail.fr>
2020-09-06 15:32:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import { SketchPicker } from "react-color";
+import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [bgCanvas] = useState("red");
+  const [color, setColor] = useState("#414141");
+  const [canvasWidth] = useState("500px");
+  const [canvasHeight] = useState("500px");
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.lineWidth = 5;
+    contextRef.current = context;
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.strokeStyle = color;
+  }, [color]);
+
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: "20px" }}>
+        <canvas
+          id="canvas"
+          onMouseDown={startDrawing}
+          onMouseUp={finishDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          // width={canvasWidth}
+          // height={canvasHeight}
+          style={{
+            border: "1px solid black",
+            cursor: "pointer",
+          }}
+        />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <SketchPicker
+          color={color}
+          onChangeComplete={(color) => setColor(color.hex)}
+        />
+      </div>
     </div>
   );
 }
